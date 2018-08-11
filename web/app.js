dae1dbc0b26edc20078d5a45c659b72d078b5c12
Mathieu PATUREL <australie.p@gmail.com>
2018-08-11 12:02:24 +1000

    fix(web): remove sample data

--- web/app.js
+++ web/app.js
@@ -1,29 +1,11 @@
-function rand(min, max, len) {
-  const arr = []
-  for (let i = 0; i <= len; i++) {
-    arr.push(Math.random() * (max - min) + min)
-  }
-  return arr
-}
-
-function range(min, max) {
-  const arr = []
-  for (let i = min; i <= max; i++) {
-    arr.push(i)
-  }
-  return arr
-}
-
-const nb = 50
-
 const chart = {
   init() {
     const ctx = document.querySelector("#chart").getContext('2d')
     const data = {
-      labels: range(0, nb),
+      labels: [],
       datasets: [
         {
-          data: rand(-nb, nb, nb),
+          data: [],
           label: 'pings',
           backgroundColor: 'rgba(50, 100, 255, 0.2)',
           borderColor: 'rgba(100, 100, 255, 0.5)'
@@ -38,7 +20,7 @@ const chart = {
   },
 
   addPings(times) {
-    last = this.chart.data.labels[this.chart.data.labels.length-1]
+    last = this.chart.data.labels[this.chart.data.labels.length-1] || 0
     for (let i = 1; i <= times.length; i++) {
       this.chart.data.labels.push(last + i)
       this.chart.data.datasets[0].data.push(times[i-1])
