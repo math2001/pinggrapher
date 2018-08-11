92779810962b5a2b81395f7341b194f73e7aad82
Mathieu PATUREL <australie.p@gmail.com>
2018-08-11 13:05:17 +1000

    feat(server): can now send to multiple clients

--- pinggrapher.go
+++ pinggrapher.go
@@ -7,19 +7,27 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
+var clients []Client
+
+type Client struct {
+	Writer  *wsutil.Writer
+	Encoder *json.Encoder
+	Conn    net.Conn
+}
+
 func read(delay int, pings chan float64) {
-	ticker := time.NewTicker(time.Duration(delay) * 1000 * 1000 * time.Nanosecond)
-	defer ticker.Stop()
+	// ticker := time.NewTicker(time.Duration(delay) * 1000 * 1000 * time.Nanosecond)
+	// defer ticker.Stop()
 	go func() {
 		var line string
 		var err error
@@ -39,6 +47,23 @@ func read(delay int, pings chan float64) {
 			pings <- ping
 		}
 	}()
+	defer func() {
+		log.Println("Close all connections")
+		for _, client := range clients {
+			client.Conn.Close()
+		}
+	}()
+	for {
+		ping := <-pings
+		for _, client := range clients {
+			if err := client.Encoder.Encode(ping); err != nil {
+				log.Print("Couldn't encode/write:", err)
+			}
+			if err := client.Writer.Flush(); err != nil {
+				log.Print("Couldn't flush:", err)
+			}
+		}
+	}
 }
 
 func startserver(port int, pings chan float64) {
@@ -49,24 +74,12 @@ func startserver(port int, pings chan float64) {
 			log.Print(err)
 			return
 		}
-		go func() {
-			defer conn.Close()
-			var (
-				w       = wsutil.NewWriter(conn, ws.StateServerSide, ws.OpText)
-				encoder = json.NewEncoder(w)
-			)
-			for {
-				ping := <-pings
-				if err := encoder.Encode(ping); err != nil {
-					log.Print(err)
-					return
-				}
-				if err := w.Flush(); err != nil {
-					log.Print(err)
-					return
-				}
-			}
-		}()
+		writer := wsutil.NewWriter(conn, ws.StateServerSide, ws.OpText)
+		clients = append(clients, Client{
+			Writer:  writer,
+			Encoder: json.NewEncoder(writer),
+			Conn:    conn,
+		})
 	})
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
