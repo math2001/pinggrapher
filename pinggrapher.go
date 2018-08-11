68ab37018190a1ae2aafea5888060d900ad56df3
Mathieu PATUREL <australie.p@gmail.com>
2018-08-11 12:17:19 +1000

    refactor(server): merge the server in one file

--- pinggrapher.go
+++ pinggrapher.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 )
 
 func read(delay int, pings chan float64) {
@@ -33,6 +39,37 @@ func read(delay int, pings chan float64) {
 			pings <- ping
 		}
 	}()
+}
+
+func startserver(port int, pings chan float64) {
+	fmt.Printf("listening on :%d\n", port)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, _, _, err := ws.UpgradeHTTP(r, w, nil)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		go func() {
+			defer conn.Close()
+			var (
+				w       = wsutil.NewWriter(conn, ws.StateServerSide, ws.OpText)
+				encoder = json.NewEncoder(w)
+			)
+			for {
+				ping := <-pings
+				if err := encoder.Encode(ping); err != nil {
+					log.Print(err)
+					return
+				}
+				if err := w.Flush(); err != nil {
+					log.Print(err)
+					return
+				}
+			}
+		}()
+	})
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
 func main() {
